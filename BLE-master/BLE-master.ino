8cc8216ea13c1f9f1b8fbfedb965a8cbc8b6298b
Christopher Michael Sutton <christopher.sutton12@gmail.com>
2020-08-26 14:54:40 +1000

    serial comm working over hm-10

---
+++ BLE-master/BLE-master.ino
@@ -0,0 +1,26 @@
+#include "BTInterface.h"
+#include <SoftwareSerial.h>
+
+SoftwareSerial blue_serial(8, 9);
+bt_interface bt_i = {4, "INIT", &blue_serial};
+char in = ' ';
+
+void setup() {
+  Serial.begin(38400);
+  initialise_interface(&bt_i);
+}
+
+void loop() {
+  comm_status rx_status = NOP;
+  
+  if (Serial.available() > 0) {
+    in = Serial.read();
+    rx_status = build_frame(&bt_i, in);
+    Serial.write(in);
+  }
+
+  if (rx_status == TRANSMIT) transmit_frame(&bt_i);
+
+  if (blue_serial.available() > 0)
+    Serial.write(blue_serial.read());
+}
---
+++ BLE-master/BTInterface.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <SoftwareSerial.h>
+#define BUFFER_SIZE 20
+#define BAUD_RATE 38400
+
+typedef struct {
+  char length;
+  char msg[BUFFER_SIZE];
+  SoftwareSerial *ss;
+} bt_interface;
+
+typedef enum { SUCCESS, FAILURE, OVERFLOW, TRANSMIT, NOP } comm_status;
+comm_status initialise_interface(bt_interface *);
+comm_status transmit_frame(bt_interface *);
+comm_status build_frame(bt_interface *);
+
+comm_status initialise_interface(bt_interface *frame) {
+  frame->ss->begin(BAUD_RATE);
+  comm_status status = transmit_frame(frame);
+};
+
+comm_status transmit_frame(bt_interface *frame) {
+  if (frame->length == 0)
+    return NOP;
+  for (int i = 0; i < frame->length && i < BUFFER_SIZE; i++) {
+    frame->ss->write(frame->msg[i]);
+  }
+  frame->ss->write("\r\n");
+  frame->length = 0;
+  return SUCCESS;
+}
+
+comm_status build_frame(bt_interface *frame, char c) {
+  if (frame->length >= BUFFER_SIZE)
+    return OVERFLOW;
+  if (c == '\r')
+    return TRANSMIT;
+  if (c == '\n')
+    return NOP;
+  frame->msg[frame->length] = c;
+  frame->length++;
+  return SUCCESS;
+}
