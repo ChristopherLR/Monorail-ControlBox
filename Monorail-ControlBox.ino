39399ef7980c28c83b4395bc00b64e2ba95fecac
thomascowan <thomas.cowan64@gmail.com>
2020-10-14 16:30:14 +1100

    LCD integration into Monorail-ControlBox.ino
    Added includes for relevant libraries, added lcd object, added initilization sequence for getting the lcd working, added a function call in the loop to display most recent information, added in the display_message function in to display each type of message

--- Monorail-ControlBox.ino
+++ Monorail-ControlBox.ino
@@ -1,7 +1,10 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 #include "BTInterface.h"
 #include <SoftwareSerial.h>
 
 SoftwareSerial blue_serial(8, 9);
+LiquidCrystal_I2C lcd(0x27, 20, 4);
 bt_interface bt_i = {4, "INIT", &blue_serial};
 char in = ' ';
 comm_status comm_state = NOP;
@@ -9,12 +12,23 @@ comm_status comm_state = NOP;
 
 void setup() {
   Serial.begin(9600);
+
+  //Initiate LCD
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("S:    >     B: ");
+  lcd.setCursor(0, 1);
+  lcd.print("D:   M:        ");
+
+  //Initiate Bluetooth module
   initialise_interface(&bt_i);
 }
 
 void loop() {
   message blue_in = read_msg();
   print_message(blue_in);
+  display_message(blue_in);
 }
 
 void serialEvent(){
@@ -83,4 +97,68 @@ void print_message(message msg) {
    default:
     Serial.println("DEFAULT");
   }  
+}
+
+/* First Line:   "S:    >     B: "
+ * Second Line:  "D:   M:        "
+ * S : Is used to indicate the current and next stations, CURR and NEXT will 
+ * be replaced by EAST/WEST corresponding to the current and next stations
+ * D : Is used to indicate the state of the door, this will either be OP = 
+ * Open or CL = Close
+ * M : Indicates the movement, this will be either ST = Start or SP = Stop
+ * B : Will be used to indicate the most recent accepted button press, 
+ * eg ST = Start, SP = Stop, E = East, W =  West, O = Open, C = Closed, 
+ * EM = Emergency
+ */
+void display_message(message msg) {
+  switch (msg) {
+    case NONE:
+      break;
+    case EAST:
+      lcd.setCursor(2, 0);
+      lcd.print("EAST");
+      lcd.setCursor(14, 0);
+      lcd.print("E");
+      break;
+    case WEST:
+      lcd.setCursor(2, 0);
+      lcd.print("WEST");
+      lcd.setCursor(14, 0);
+      lcd.print("W");
+      break;
+    case START:
+      lcd.setCursor(7, 1);
+      lcd.print("ST");
+      lcd.setCursor(14, 0);
+      lcd.print("ST");
+      break;
+    case STOP:
+      lcd.setCursor(7, 1);
+      lcd.print("SP");
+      lcd.setCursor(14, 0);
+      lcd.print("SP");
+      break;
+    case OPEN:
+      lcd.setCursor(2, 1);
+      lcd.print("OP");
+      lcd.setCursor(14, 0);
+      lcd.print("OP");
+      break;
+    case CLOSE:
+      lcd.setCursor(2, 1);
+      lcd.print("CL");
+      lcd.setCursor(14, 0);
+      lcd.print("CL");
+      break;
+    case EMERGENCY:
+      lcd.setCursor(14, 0);
+      lcd.print("EM");
+      lcd.setCursor(0, 0);
+      lcd.print("Emergrency stop ");
+      lcd.setCursor(0, 1);
+      lcd.print("Please Reset");
+      break;
+    default:
+      break;
+  }
 }
